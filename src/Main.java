8b9b9349dc7f389382dbf45295b6b7c3e71562d1
Rawan <Rawan.cheaito369@hotmail.com>
2023-05-31 22:49:19 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+//write a java project that allows the user to
+//        manage the visa card (create card, delete card, credit, debit)-
+//        you may have more than one card-
+//        if the visa card was of type silver printf
+//        before every debit or credit operation "Silver" and
+//        if the visa was of type Gold print also "Gold"
+//
+//        in the main create a menu
+//        that allows the user to create , delete ,
+//        credit -add amount- and debit -remove amount if possible
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        }
+    }
