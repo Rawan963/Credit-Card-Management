5e654137de01ba68f7e4e46b785a2d253beacf1c
Rawan <Rawan.cheaito369@hotmail.com>
2023-06-03 22:03:26 +0300

    edited and added

--- src/Main.java
+++ src/Main.java
@@ -15,18 +15,16 @@ public class Main {
         while (true) {
             System.out.println("Select an option");
             System.out.println("1. create visaCard");
-            System.out.println("2. credit to a visaCard");
-            System.out.println("3. debit from a visaCard");
-            System.out.println("4. delete a visaCard");
+            System.out.println("2. delete a visaCard");
+            System.out.println("3. credit to a visaCard");
+            System.out.println("4. debit from a visaCard");
             System.out.println("5. exit");
 
             int option;
             option = scanner.nextInt();
             scanner.nextLine();
             switch (option) {
-                case 0:
-                    System.out.println("Exiting program...");
-                    return;
+
                 case 1:
                     createVisaCard();
                     break;
@@ -39,14 +37,15 @@ public class Main {
                 case 4:
                     debitAmount();
                     break;
+                case 5:
+                    System.out.println("Exiting");
+                    return;
                 default:
                     System.out.println("Invalid option. Please try again.");
             }
 
         }
     }
-
-
 
     private static void createVisaCard() {
         {
@@ -55,9 +54,9 @@ public class Main {
             System.out.println("Enter card id");
             int id = scanner.nextInt();
             if (cardType.equalsIgnoreCase("silver")) {
-                VisaCardManager.createCard(new SilverVisaCard(id));
+                VisaCardList.createCard(new SilverVisaCard(id));
             } else if (cardType.equalsIgnoreCase("gold")) {
-                VisaCardManager.createCard(new GoldenVisaCard(id));
+                VisaCardList.createCard(new GoldenVisaCard(id));
             } else {
                 System.out.println("Please choose 'silver' or 'gold'.");
             }
@@ -80,5 +79,50 @@ public class Main {
         if (!cardFound) {
             System.out.println("No Visa card found with ID ");
         }
+        else{
+            System.out.println("Visa card deleted");
+        }
     }
+
+    private static void creditAmount() {
+        System.out.println("Enter card id");
+        int id = scanner.nextInt();
+        System.out.println("Enter amount to be added:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        List<VisaCard> visaCards = VisaCardList.getVisaCards();
+        boolean cardFound = false;
+        for (int i = 0; i < visaCards.size(); i++) {
+            VisaCard card = visaCards.get(i);
+            if (card.getID() == id) {
+                card.credit(amount);
+                cardFound = true;
+                break;
+            }
+        }
+        if (!cardFound) {
+            System.out.println("No Visa card found with ID ");
+        }
+    }
+    private static void debitAmount() {
+        System.out.println("Enter card id");
+        int id = scanner.nextInt();
+        System.out.println("Enter amount to be removed:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        List<VisaCard> visaCards = VisaCardList.getVisaCards();
+        boolean cardFound = false;
+        for (int i = 0; i < visaCards.size(); i++) {
+            VisaCard card = visaCards.get(i);
+            if (card.getID() == id) {
+                card.debit(amount);
+                cardFound = true;
+                break;
+            }
+        }
+        if (!cardFound) {
+            System.out.println("No Visa card found with ID ");
+        }
+    }
+
 }
--- src/VisaCardManager.java
+++ src/VisaCardManager.java
@@ -12,16 +12,8 @@ public class VisaCardManager {
         visaCards.add(VisaCardname);
         System.out.println("Visa card created ");
     }
-    public void deleteCard(VisaCard VisaCardname){
-        visaCards.remove(VisaCardname);
-        System.out.println("Visa card deleted ");
-    }
-    public void creditCard(VisaCard visaCard, double amount) {
-        visaCard.credit(amount);
-    }
-    public void debitCard(VisaCard visaCard, double amount) {
-        visaCard.debit(amount);
-    }
+
+
     public List<VisaCard> getVisaCards() {
         return visaCards;
     }
