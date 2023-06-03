d9114b0e7fe41ae444486bd6de17193dd9efb6d7
Rawan <Rawan.cheaito369@hotmail.com>
2023-06-03 21:39:50 +0300

    edited and added

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
+import java.util.List;
 import java.util.Scanner;
-
-
 public class Main {
     private static VisaCardManager VisaCardList;
-    private static Scanner scanner=new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        VisaCardList =new VisaCardManager();
+        VisaCardList = new VisaCardManager();
         displayMenu();
 
     }
 
-    private static void displayMenu(){
-        while(true){
+    private static void displayMenu() {
+        while (true) {
             System.out.println("Select an option");
             System.out.println("1. create visaCard");
             System.out.println("2. credit to a visaCard");
@@ -21,9 +20,9 @@ public class Main {
             System.out.println("4. delete a visaCard");
             System.out.println("5. exit");
 
-           int option;
-           option= scanner.nextInt();
-           scanner.nextLine();
+            int option;
+            option = scanner.nextInt();
+            scanner.nextLine();
             switch (option) {
                 case 0:
                     System.out.println("Exiting program...");
@@ -44,33 +43,42 @@ public class Main {
                     System.out.println("Invalid option. Please try again.");
             }
 
-
         }
     }
 
 
 
-    private static void createVisaCard(){{
-        System.out.println("Enter card type (silver/gold):");
-        String cardType = scanner.nextLine();
-        System.out.println("Enter card id");
-       int id = scanner.nextInt();
-        if (cardType.equalsIgnoreCase("silver")) {
-            VisaCardManager.createCard(new SilverVisaCard(id));
-        } else if (cardType.equalsIgnoreCase("gold")) {
-            VisaCardManager.createCard(new GoldenVisaCard(id));
-        } else {
-            System.out.println("Please choose 'silver' or 'gold'.");
+    private static void createVisaCard() {
+        {
+            System.out.println("Enter card type (silver/gold):");
+            String cardType = scanner.nextLine();
+            System.out.println("Enter card id");
+            int id = scanner.nextInt();
+            if (cardType.equalsIgnoreCase("silver")) {
+                VisaCardManager.createCard(new SilverVisaCard(id));
+            } else if (cardType.equalsIgnoreCase("gold")) {
+                VisaCardManager.createCard(new GoldenVisaCard(id));
+            } else {
+                System.out.println("Please choose 'silver' or 'gold'.");
+            }
         }
     }
-    }
+
     private static void deleteVisaCard() {
         System.out.println("Enter card id");
         int id = scanner.nextInt();
-
-
-
-
+        List<VisaCard> visaCards = VisaCardList.getVisaCards();
+        boolean cardFound = false;
+        for (int i = 0; i < visaCards.size(); i++) {
+            VisaCard card = visaCards.get(i);
+            if (card.getID() == id) {
+                visaCards.remove(i);
+                cardFound = true;
+                break;
+            }
+        }
+        if (!cardFound) {
+            System.out.println("No Visa card found with ID ");
+        }
     }
-
 }
