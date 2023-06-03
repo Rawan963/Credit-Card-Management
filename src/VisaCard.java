222544d2643405c73054a2d3defd08c5b30c05fd
Rawan <Rawan.cheaito369@hotmail.com>
2023-06-03 18:43:43 +0300

    added

--- src/VisaCard.java
+++ src/VisaCard.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class VisaCard {
     private int id;
     private String type;
---
+++ src/VisaCardManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisaCardManager {
+    private List<VisaCard> visaCards;
+
+    public VisaCardManager() {
+        this.visaCards = new ArrayList<>();
+    }
+}
