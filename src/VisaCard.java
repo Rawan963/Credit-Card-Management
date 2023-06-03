14a0172fc807bb708c25d6b3644e9313302c7612
Rawan <Rawan.cheaito369@hotmail.com>
2023-06-03 18:39:30 +0300

    added

--- src/VisaCard.java
+++ src/VisaCard.java
@@ -25,21 +25,9 @@ public abstract class VisaCard {
         }
 
 
-    public void credit(double amount, int id) {
-        setBalance(this.getBalance()+amount);
-    }
+    public abstract void credit(double amount);
 
-    public void debit(double amount,int id){
-        if(amount>this.getBalance())
-            return;
-        setBalance(this.getBalance()-amount);
-    }
+    public abstract void debit(double amount);
 
 }
 
- class SilverVisaCard extends VisaCard {
-    public SilverVisaCard(int id) {
-        super("Silver", id);
-    }
-
- }
